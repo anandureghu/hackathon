d69b74a5b6d86949e3b9a50b7c1343f8fd6f8f99
kavya <kavya.karayil@qburst.com>
2024-05-13 14:33:48 +0530

    Initail setup

---
+++ src/app/scroll/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./scroll.module.scss";
+
+export default function Scroll() {
+  return (
+    <>
+      <div className={styles.red_container}></div>
+      <div className={styles.green_container}></div>
+      <div className={styles.blue_container}></div>
+    </>
+  );
+}
