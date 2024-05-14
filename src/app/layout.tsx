156df6f720f88d4257374e5bea7c97ba7112da2e
kavya <kavya.karayil@qburst.com>
2024-05-14 15:58:20 +0530

    Added styles folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import "./index.scss";
+
 export default function RootLayout({
   children,
 }: {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import styles from "./page.module.scss";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div className={styles.main_container}>
+      <div className={styles.button_container}>
+        <button type="button" className={styles.button}>
+          REGISTER NOW
+        </button>
+      </div>
+    </div>
+  );
 }
