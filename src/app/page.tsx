6c76da84adc1269cd32fdf66d48f861a74ffd9cc
kavya <kavya.karayil@qburst.com>
2024-05-14 21:33:55 +0530

    autentication using clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
+"use client";
+import { useUser, UserButton } from "@clerk/nextjs";
 import styles from "./page.module.scss";
+import Link from "next/link";
 
 export default function Page() {
+  const { user, isLoaded } = useUser();
+  console.log({ user });
+
   return (
     <div className={styles.main_container}>
       <div className={styles.button_container}>
-        <button type="button" className={styles.button}>
+        {isLoaded && user ? (
+          <>
+            <button type="button" className={styles.button}>
+              LOGOUT
+            </button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        ) : (
+          <Link href="/sign-in" className={styles.button}>
+            LOGIN
+          </Link>
+        )}
+        {/* <button type="button" className={styles.button}>
           REGISTER NOW
-        </button>
+        </button> */}
       </div>
     </div>
   );
